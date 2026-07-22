21b3a67efe73c77987251c19dfabc5b87d299502
ndgcreates <ndgcreates@gmail.com>
2026-07-22 11:05:58 -0700

    Deploy: in-app quiz creator

---
+++ _next/static/5QLvwhwxKIOPFwr72uv4t/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fquiz\u002F[benchmarkId]","\u002Freview\u002F[bigIdeaId]","\u002Freview\u002F[bigIdeaId]\u002F[benchmarkId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
